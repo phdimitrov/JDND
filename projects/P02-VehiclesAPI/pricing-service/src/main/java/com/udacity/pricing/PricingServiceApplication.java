8be72c4404c3e589cf32ae037834dd18729fa4de
Philip <phd@foryouandyourcustomers.com>
2020-12-27 10:34:38 +0200

    Convert the Pricing Service

--- projects/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ projects/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,16 +2,28 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
+    }
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{new ClassPathResource("prices.json")});
+        return factory;
     }
 
 }
--- projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,7 +1,13 @@
 package com.udacity.pricing;
 
+import static org.junit.Assert.assertEquals;
+
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +15,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
+	@Autowired
+	private PriceRepository priceRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void checkPriceJsonPopulator() {
+		List<Price> prices = priceRepository.findAll();
+		assertEquals("Expecting 20", 20, prices.size());
+
+		Price price = prices.get(0);
+		assertEquals(1L, price.getVehicleId().longValue());
+		assertEquals("15189.51", price.getPrice().toString());
+		assertEquals("USD", price.getCurrency());
 	}
 
 }
